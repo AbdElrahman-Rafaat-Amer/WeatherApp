635d0fa5306057d76de22533a9f2781b3651e8d0
Abdelrahman Raafat <abdelrahman.raafaat@gmail.com>
2024-07-30 02:24:04 +0300

    [CC-5] Change App package name
    - Changed App package name.
    - Changed App name.

---
+++ app/src/main/java/com/abdelrahman/raafat/climateClue/model/RepositoryInterface.kt
@@ -0,0 +1,38 @@
+package com.abdelrahman.raafat.climateClue.model
+
+
+interface RepositoryInterface {
+
+    //Network
+    suspend fun getWeatherFromNetwork(
+        latitude: String,
+        longitude: String,
+        language: String
+    ): WeatherResponse
+
+    //Room
+
+    //Current
+    suspend fun getWeatherFromDataBase(): WeatherResponse?
+    suspend fun insertCurrentDataToRoom(weatherResponse: WeatherResponse)
+
+
+    //Address
+    suspend fun getStoredPlace(): SavedAddress
+    suspend fun insertAddressToRoom(address: SavedAddress)
+
+
+    //Favorites
+    suspend fun getFavoriteFromDataBase(): List<FavoritePlaces>
+    suspend fun insertToFavorite(favoritePlaces: FavoritePlaces)
+    suspend fun removeFromFavorite(favoritePlaces: FavoritePlaces) : Int
+
+
+    //Alerts
+    suspend fun insertAlertToRoom(savedAlerts: SavedAlerts): Long
+    suspend fun getStoredAlerts(): List<SavedAlerts>
+    suspend fun deleteAlertFromRoom(id: Int): Int
+    suspend fun getAlertFromRoom(id: Int): SavedAlerts
+
+
+}
